40bcdb9902b78772e5c0727817bdb2c357cf0e87
Tang <ztang@bamdev2.elzar.cshl.edu>
2022-11-10 13:29:36 -0500

    roberta and bert run code update for dataset choices

--- rna_self_train/kmer_roberta.py
+++ rna_self_train/kmer_roberta.py
@@ -8,15 +8,27 @@ from dna_tokenizer import rnabert_maskwrapper,DNATokenizer
 import os
 os.environ['WANDB_PROJECT'] = 'rna_MLM'
 os.environ['WANDB_LOG_MODEL'] = 'true'
-####Modify for logging and saving information###
-decay_rate = 0.15
-run_name = '6mer_dr0.15_roberta_v0'
+
+### RUN_DATASET_TYPE
+#runtype = 'context'
+runtype = '6mer'
 
 tokenizer = DNATokenizer('./rna_self_train/vocab.txt')
-data_dir = './data/pre-train/510_6/rna_seq.h5'
-train_data = rna_model.rna_long_kmer(data_dir,'train',6,tokenizer)
-valid_data = rna_model.rna_long_kmer(data_dir,'valid',6,tokenizer)
-data_collator = rnabert_maskwrapper(tokenizer,decay_rate,extend = False)
+if runtype == 'context':
+    decay_rate=0.05
+    data_dir = './data/pre-train/510/rna_seq.h5'
+    train_data = rna_model.rna_kmer(data_dir,'train',6,tokenizer)
+    valid_data = rna_model.rna_kmer(data_dir,'valid',6,tokenizer)
+    data_collator = rnabert_maskwrapper(tokenizer,decay_rate,extend = True)
+
+elif runtype == '6mer':
+    decay_rate = 0.15
+    data_dir = './data/pre-train/510_6/rna_seq.h5'
+    train_data = rna_model.rna_long_kmer(data_dir,'train',6,tokenizer)
+    valid_data = rna_model.rna_long_kmer(data_dir,'valid',6,tokenizer)
+    data_collator = rnabert_maskwrapper(tokenizer,decay_rate,extend = False)
+
+run_name = runtype+'_dr'+str(decay_rate)+'_roberta_v0'
 
 # Initializing a RoBERTa configuration
 configuration = RobertaConfig(
@@ -37,9 +49,9 @@ if args.local_rank == 0:
     training_args = TrainingArguments(
         output_dir = './wandb/'+run_name,
         run_name = run_name,
-        num_train_epochs=30,
+        num_train_epochs=70,
         do_train=True,
-        learning_rate = 1e-03,
+        learning_rate = 6e-04,
         adam_beta1 = 0.9,
         adam_beta2 = 0.98,
         adam_epsilon = 1e-06,
@@ -74,7 +86,7 @@ else:
         run_name = run_name,
         num_train_epochs=30,
         do_train=True,
-        learning_rate = 1e-03,
+        learning_rate = 6e-04,
         adam_beta1 = 0.9,
         adam_beta2 = 0.98,
         adam_epsilon = 1e-06,
@@ -84,7 +96,7 @@ else:
         evaluation_strategy = 'steps',
         gradient_accumulation_steps = 40,
         per_device_train_batch_size=32,
-        logging_steps = 50,
+        logging_steps = 80,
         eval_steps = 500,
         save_total_limit=2,
         ddp_find_unused_parameters = False,
