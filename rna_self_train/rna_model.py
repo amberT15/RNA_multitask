7d60fbeab3f5a932e007ec51799e4e49c7dd2446
amberT15 <amber.zqt@gmail.com>
2023-01-30 08:25:23 -0500

    training code for custom convfomer model

--- rna_self_train/rna_model.py
+++ rna_self_train/rna_model.py
@@ -15,8 +15,208 @@ from sequence_models.layers import PositionFeedForward
 from sequence_models.losses import MaskedCrossEntropyLoss
 from sequence_models.collaters import  SimpleCollater,_pad
 from sequence_models.constants import PAD,ALL_AAS,MASK,START,STOP
+from custom_longformer import LongformerSelfAttention as longformer_attention
+
+#test saving
+############ customized conv + longformer model ############
+class conv_former_config():
+    def __init__(self, hidden_size,attention_window,
+                num_attention_heads,
+                intermediate_size,
+                masking_rate = 0.15,
+                attention_dilation = 1,
+                attention_probs_dropout_prob = 0,
+                attention_mode = 'sliding_chunks',
+                autoregressive = False,
+                layer_norm_eps=1e-12,
+                hidden_dropout_prob=0.1,
+                output_attentions=False,
+                output_hidden_states = False,
+                vocab_size = 4,learning_rate=0.0005,
+                warm_up_step = 5000, **kwargs):
+        # required regular parameters
+        self.hidden_size = hidden_size
+        self.attention_window = attention_window
+        self.num_attention_heads = num_attention_heads
+        self.intermediate_size = intermediate_size
+        self.masking_rate = masking_rate
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.attention_mode = attention_mode
+        self.attention_dilation = attention_dilation
+        self.autoregressive = autoregressive
+        self.layer_norm_eps = layer_norm_eps
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.output_attentions = output_attentions
+        self.output_hidden_states = output_hidden_states
+        self.vocab_size = vocab_size
+        self.learning_rate = learning_rate
+        self.warm_up_step = warm_up_step
+        # other parameters
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+class longformer_block(pl.LightningModule):
+     def __init__(self,config,layer_id):
+        super(longformer_block,self).__init__()
+        self.attention = longformer_attention(config,layer_id)
+        self.linear_project = nn.Linear(config.hidden_size,config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.layernorm = nn.LayerNorm(config.hidden_size,eps=config.layer_norm_eps)
+        self.ff1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.activation = nn.GELU()
+        self.ff2 = nn.Linear(config.intermediate_size, config.hidden_size)
+        self.drop2 = nn.Dropout(config.hidden_dropout_prob)
+        self.layernorm2 = nn.LayerNorm(config.hidden_size,eps=config.layer_norm_eps)
+
+        self.output_attentions = config.output_attentions
 
 
+     def forward(self,hidden_state):
+        attn = self.attention(hidden_state,output_attentions=self.output_attentions)
+        additional_output = attn[1:]
+        attn = self.linear_project(attn[0])
+        attn = self.dropout(attn)
+        atten_out = self.layernorm(attn + hidden_state)
+        ff_out = self.ff1(atten_out)
+        ff_out = self.activation(ff_out)
+        ff_out = self.ff2(ff_out)
+        ff_out = self.drop2(ff_out)
+        layer_output = self.layernorm2(ff_out+atten_out)
+        outputs = (layer_output,) + additional_output
+        return outputs
+
+class conv_former(pl.LightningModule):
+    def __init__(self,config):
+        super(conv_former,self).__init__()
+        #Learning rate and warmup/decay setting
+        self.lr = config.learning_rate
+        self.warmup = config.warm_up_step
+        self.mask = config.masking_rate
+        #output parameters
+        self.output_attentions = config.output_attentions
+        self.output_hidden_states = config.output_hidden_states
+
+        #conv input head
+        self.conv1 = nn.Conv1d(4,config.hidden_size,19,padding='same')
+        self.batchnorm = nn.BatchNorm1d(config.hidden_size)
+        self.block1 = dilated_residual(config.hidden_size,7,[2],0.1)
+        self.block2 = dilated_residual(config.hidden_size,7,[4],0.1)
+
+        #longformer attention layers
+        self.att_list = nn.ModuleList(
+            [longformer_block(config,i) for i in range(0,3)]
+        )
+
+        #MLM classifcation head
+        self.output_head = nn.Sequential(
+          nn.Linear(config.hidden_size,config.hidden_size),
+          nn.GELU(),
+          nn.LayerNorm(config.hidden_size,eps=config.layer_norm_eps),
+        )
+
+        self.vocab_linear = nn.Linear(config.hidden_size,config.vocab_size)
+        self.vocab_bias = nn.Parameter(torch.zeros(config.vocab_size))
+        self.vocab_linear.bias = self.vocab_bias
+
+        #metrics
+        self.loss_func = nn.CrossEntropyLoss(reduction = 'none')
+
+    def forward(self, x):
+        all_hidden_state = () if self.output_hidden_states else None
+        all_attentions = () if self.output_attentions else None
+
+        #conv head
+        conv = self.conv1(x)
+        conv = self.batchnorm(conv)
+        res = self.block1(conv)
+        hidden_states = self.block2(res)
+        hidden_states = hidden_states.transpose(1, 2)
+
+        #attention blocks
+        for i, att_layer in enumerate(self.att_list):
+            if self.output_hidden_states:
+                all_hidden_state = all_hidden_state + (hidden_states,)
+            layer_outputs = att_layer(hidden_states)
+            hidden_states = layer_outputs[0]
+            if self.output_attentions:
+                all_attentions = all_attentions + (layer_outputs[1],)
+
+        if self.output_hidden_states:
+                all_hidden_state = all_hidden_state + (hidden_states,)
+
+        #classification output head
+        output_vec = self.output_head(hidden_states)
+        output_logits = self.vocab_linear(output_vec)
+        output_logits = output_logits.transpose(1,2)
+        return output_logits
+
+    def training_step(self, x, batch_idx):
+        masked_x,mask_idx = onehot_collator(x,self.mask)
+        pred_x = self(masked_x)
+
+        loss =self.loss_func(pred_x, x)
+        loss[mask_idx] = 0
+        loss = loss.sum()
+
+        self.log("train_loss", loss,on_step = True, on_epoch = False)
+        return loss
+
+    def validation_step(self, x, batch_idx):
+        masked_x,mask_idx = onehot_collator(x,self.mask)
+        pred_x = self(masked_x)
+
+        loss =self.loss_func(pred_x, x)
+        loss[mask_idx] = 0
+        loss = loss.sum()
+
+        self.log("val_loss", loss,on_step = True, on_epoch = False)
+        return loss
+
+    def configure_optimizers(self):
+        self.opt = torch.optim.AdamW(self.parameters(),lr=self.lr)
+        self.reduce_lr = torch.optim.lr_scheduler.ReduceLROnPlateau(self.opt,
+                                                                     mode = 'min',
+                                                                     factor = 0.2,
+                                                                    patience = 5,
+                                                                    min_lr = 1e-7,
+                                                                    verbose = True)
+        schedulers =  {'scheduler':self.reduce_lr,'monitor':"val_loss",}
+        return [self.opt], [schedulers]
+
+    def optimizer_step(self, epoch_nb, batch_nb, optimizer, optimizer_i, opt_closure):
+        # update params
+        optimizer.step(closure=optimizer_closure)
+
+        # manually warm up lr without a scheduler
+        if self.trainer.global_step < self.warmup:
+            lr_scale = min(1.0, float(self.trainer.global_step + 1) / self.warmup)
+            for pg in optimizer.param_groups:
+                pg["lr"] = lr_scale * self.lr
+
+def onehot_collator(onehot_seq,p=0.15):
+    N,_,L = onehot_seq.shape
+    out_seq = onehot_seq.clone()
+    uniform_distribution = np.random.rand(N,L)
+    masked_index = np.where(uniform_distribution<0.15)
+    out_seq[masked_index[0],:,masked_index[1]] = torch.cuda.FloatTensor([0,0,0,0])
+
+    return out_seq,masked_index
+
+class longformer_dataset(Dataset):
+    def __init__(self,h5_path,dataset):
+        self.set = dataset
+        self.h5_file = h5py.File(h5_path, "r")
+
+    def __len__(self):
+        return len(self.h5_file[self.set])
+
+    def __getitem__(self,index):
+        input_key = self.set
+        inputs = self.h5_file[input_key][index].astype(np.float32)
+        return inputs
+
+######################Dataset / Collater ###############################
 class BertCollater(SimpleCollater):
     """Collater for masked language sequence models.
 
@@ -68,7 +268,7 @@ class BertCollater(SimpleCollater):
         src = _pad(src, pad_idx)
         tgt = _pad(tgt, pad_idx)
         mask = _pad(mask, 0)
-        
+
         label_mask = (mask==0)
         tgt[label_mask] = -100
         #return src, tgt, mask
@@ -119,8 +319,8 @@ class rna_self_mask(Dataset):
     def __getitem__(self,index):
         seq = self.h5_file[index]
         list_seq = seq.decode("utf-8")
-        token_seq = self.tokenizer.encode([*list_seq], 
-                                        add_special_tokens=True, 
+        token_seq = self.tokenizer.encode([*list_seq],
+                                        add_special_tokens=True,
                                         max_length=self.maxl)
         return np.squeeze(token_seq)
 
@@ -138,8 +338,8 @@ class rna_context(Dataset):
         seq = self.h5_file[index]
         list_seq = seq.decode("utf-8")
         split_seq = ' '.join(list_seq[i:i+self.kmer] for i in range(0, len(list_seq)-self.kmer+1, 1))
-        token_seq = self.tokenizer.encode(split_seq, 
-                                        add_special_tokens=True, 
+        token_seq = self.tokenizer.encode(split_seq,
+                                        add_special_tokens=True,
                                         max_length=self.maxl)
         return np.squeeze(token_seq)
 
@@ -157,10 +357,12 @@ class rna_kmer(Dataset):
         seq = self.h5_file[index]
         list_seq = seq.decode("utf-8")
         split_seq = ' '.join(list_seq[i*self.kmer :(i+1)*self.kmer] for i in range(0, int(len(list_seq)/self.kmer)))
-        token_seq = self.tokenizer.encode(split_seq, 
-                                        add_special_tokens=True, 
+        token_seq = self.tokenizer.encode(split_seq,
+                                        add_special_tokens=True,
                                         max_length=self.maxl)
         return np.squeeze(token_seq)
+
+################### Model ###################
 
 class ByteNetLM(pl.LightningModule):
 
@@ -195,7 +397,7 @@ class ByteNetLM(pl.LightningModule):
         y_hat = self(x)
         loss =self.loss_func(y_hat, y, mask)
         self.log("train_loss", loss,on_step = False, on_epoch = True, sync_dist=True)
-        
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -203,7 +405,7 @@ class ByteNetLM(pl.LightningModule):
         y_hat = self(x)
         loss =self.loss_func(y_hat, y, mask)
         self.log("val_loss", loss,on_step = False, on_epoch = True, sync_dist=True)
-        
+
         return loss
     def configure_optimizers(self):
         self.opt=torch.optim.Adam(self.parameters(), lr=self.lr)
